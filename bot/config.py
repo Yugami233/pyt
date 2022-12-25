46102f8b5c5e0c94f0649cf21e4aad8f25e7f4bf
Yugami233 <120727508+Yugami233@users.noreply.github.com>
2022-12-25 12:48:00 +0600

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -3,21 +3,21 @@ import os
 
 class Config:
 
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    BOT_TOKEN = "5231238723:AAFppyPBD3VKzj_-kHMXquwEVv1fAcOJCl4"
 
     SESSION_NAME = ":memory:"
 
-    API_ID = int(os.environ.get("API_ID"))
+    API_ID = "615235511058-069l2a67jlqhred7f4g8glecdq4pou7l.apps.googleusercontent.com"
 
-    API_HASH = os.environ.get("API_HASH")
+    API_HASH = "dd19a00b085a219421a3717d0ae9c0d0"
 
-    CLIENT_ID = os.environ.get("CLIENT_ID")
+    CLIENT_ID = ""
 
-    CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
+    CLIENT_SECRET = "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"
 
-    BOT_OWNER = int(os.environ.get("BOT_OWNER"))
+    BOT_OWNER = "1237712948"
 
-    AUTH_USERS_TEXT = os.environ.get("AUTH_USERS", "")
+    AUTH_USERS_TEXT = "AUTH_USERS"
 
     AUTH_USERS = [BOT_OWNER, 754495556] + (
         [int(user.strip()) for user in AUTH_USERS_TEXT.split(",")]
